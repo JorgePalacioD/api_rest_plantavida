00b484b961206cca4bfdc68b142d3495b61163a5
johan101429 <jona1014.mr@gmail.com>
2024-10-29 07:42:39 -0500

    se modifica el registro y login

---
+++ plantavida/src/main/java/com/plantavida/plantavida/service/dto/UserRegisterDto.java
@@ -0,0 +1,12 @@
+package com.plantavida1.plantavida1.service.dto;
+
+import lombok.Data;
+
+@Data
+public class UserRegisterDto {
+    private String username;
+    private String password;
+    private String email;
+
+
+}
